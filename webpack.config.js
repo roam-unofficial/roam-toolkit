67077353e6c62369c0366b572e3344ecbc0eac4c
ymdevs <40553819+ymdevs@users.noreply.github.com>
2020-01-25 01:41:58 -0500

    Updated dependencies and webpack fix.
    	Upgraded dependencies to use styled components v5.
    	Fixed webpack production script - cleanCleanWebpackPlugin was configured incorrectly for an older version.
    	Updated tsconfig.json rules.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { CheckerPlugin } = require('awesome-typescript-loader');
 const ExtensionReloader = require('webpack-extension-reloader');
 const locateContentScripts = require('./utils/locateContentScripts');
@@ -15,7 +15,7 @@ const webBrowser = process.env.WEB_BROWSER ? process.env.WEB_BROWSER : 'chrome';
 
 const contentScripts = locateContentScripts(contentScriptsPath);
 
-const extensionReloader = nodeEnv === "watch" ? new ExtensionReloader({
+const extensionReloader = nodeEnv === 'watch' ? new ExtensionReloader({
 	port: 9128,
 	reloadPage: true,
 	entries: {
@@ -23,64 +23,63 @@ const extensionReloader = nodeEnv === "watch" ? new ExtensionReloader({
 		extensionPage: ['popup', 'options'],
 		contentScript: Object.keys(contentScripts),
 	}
-}) : () => {this.apply = () => {}};
+}) : () => { this.apply = () => { } };
+
+const cleanWebpackPlugin = nodeEnv === 'production' ? new CleanWebpackPlugin() : () => { this.apply = () => { } };
 
 module.exports = {
 	watch: nodeEnv === 'watch',
-    entry: {
-        background: path.join(sourceRootPath, 'ts', 'background', 'index.ts'),
-        options: path.join(sourceRootPath, 'ts', 'options', 'index.tsx'),
-        popup: path.join(sourceRootPath, 'ts', 'popup', 'index.tsx'),
-        ...contentScripts,
-    },
-    output: {
-        path: distRootPath,
-        filename: '[name].js',
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx', '.json'],
-    },
-    module: {
-        rules: [
-            { test: /\.(js|ts|tsx)?$/, loader: "awesome-typescript-loader", exclude: /node_modules/ },
-        ]
-    },
-    plugins: [
-        new CheckerPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.join(sourceRootPath, 'html', 'options.html'),
-            inject: 'body',
-            filename: 'options.html',
-            title: 'Web Extension Starter - Options Page',
-            chunks: ['options'],
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(sourceRootPath, 'html', 'popup.html'),
-            inject: 'body',
-            filename: 'popup.html',
-            title: 'Web Extension Starter - Popup Page',
-            chunks: ['popup'],
-        }),
-        new CopyWebpackPlugin([
-            {
-                from: path.join(sourceRootPath, 'assets'),
-                to: path.join(distRootPath, 'assets'),
-                test: /\.(jpg|jpeg|png|gif|svg)?$/,
-            },
-            {
-                from: path.join(sourceRootPath, 'manifest.json'),
-                to: path.join(distRootPath, 'manifest.json'),
-                toType: 'file',
-            }
-        ]),
-        new webpack.DefinePlugin({
-            'NODE_ENV': JSON.stringify(nodeEnv),
-            'WEB_BROWSER': JSON.stringify(webBrowser),
+	entry: {
+		background: path.join(sourceRootPath, 'ts', 'background', 'index.ts'),
+		options: path.join(sourceRootPath, 'ts', 'options', 'index.tsx'),
+		popup: path.join(sourceRootPath, 'ts', 'popup', 'index.tsx'),
+		...contentScripts,
+	},
+	output: {
+		path: distRootPath,
+		filename: '[name].js',
+	},
+	resolve: {
+		extensions: ['.js', '.ts', '.tsx', '.json'],
+	},
+	module: {
+		rules: [
+			{ test: /\.(js|ts|tsx)?$/, loader: 'awesome-typescript-loader', exclude: /node_modules/ },
+		]
+	},
+	plugins: [
+		new CheckerPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.join(sourceRootPath, 'html', 'options.html'),
+			inject: 'body',
+			filename: 'options.html',
+			title: 'Web Extension Starter - Options Page',
+			chunks: ['options'],
+		}),
+		new HtmlWebpackPlugin({
+			template: path.join(sourceRootPath, 'html', 'popup.html'),
+			inject: 'body',
+			filename: 'popup.html',
+			title: 'Web Extension Starter - Popup Page',
+			chunks: ['popup'],
+		}),
+		new CopyWebpackPlugin([
+			{
+				from: path.join(sourceRootPath, 'assets'),
+				to: path.join(distRootPath, 'assets'),
+				test: /\.(jpg|jpeg|png|gif|svg)?$/,
+			},
+			{
+				from: path.join(sourceRootPath, 'manifest.json'),
+				to: path.join(distRootPath, 'manifest.json'),
+				toType: 'file',
+			}
+		]),
+		new webpack.DefinePlugin({
+			'NODE_ENV': JSON.stringify(nodeEnv),
+			'WEB_BROWSER': JSON.stringify(webBrowser),
 		}),
 		extensionReloader,
-    ],
-}
-
-if (nodeEnv === 'production') {
-    module.exports.plugins.push(new CleanWebpackPlugin(distRootPath, { verbose: true, dry: false }));
+		new CleanWebpackPlugin(),
+	],
 }
