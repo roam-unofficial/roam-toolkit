8147c5e984e2494382a8de298047c12b795c9078
Vladyslav Sitalo <sitalov@amazon.com>
2020-01-29 00:32:31 -0800

    Make SRS actually work. (Use value vs NodeValue)

--- src/ts/contentScripts/counter/index.tsx
+++ src/ts/contentScripts/counter/index.tsx
@@ -14,12 +14,13 @@ const bucketExpr = /\[\[Bucket (\d+)]]/;
 const nextBucket = (nodeStr: string) => `[[Bucket ${parseInt(nodeStr) + 1}]]`;
 
 function triggerNextBucket() {
-    const element = document.activeElement!;
+    const element = getRealEdit()!;
+    console.log(element);
     if (element.tagName.toLocaleLowerCase() !== 'textarea') {
         return
     }
 
-    element.nodeValue = element.nodeValue!.replace(bucketExpr, (_, numStr: string) => nextBucket(numStr));
+    element.value = element.value!.replace(bucketExpr, (_, numStr: string) => nextBucket(numStr));
     element.dispatchEvent(inputEvent);
 }
 
@@ -29,3 +30,26 @@ browser.runtime.onMessage.addListener((command) => {
         triggerNextBucket();
     }
 });
+
+type ValueElement = HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement;
+
+function getRealEdit(): ValueElement {
+    // stolen from Surfingkeys. Needs work.
+
+    let rt = document.activeElement;
+    // on some pages like chrome://history/, input is in shadowRoot of several other recursive shadowRoots.
+    while (rt && rt.shadowRoot) {
+        if (rt.shadowRoot.activeElement) {
+            rt = rt.shadowRoot.activeElement;
+        } else if (rt.shadowRoot.querySelector('input, textarea, select')) {
+            rt = rt.shadowRoot.querySelector('input, textarea, select');
+            break;
+        } else {
+            break;
+        }
+    }
+    // if (rt === window) {
+    //     rt = document.body;
+    // }
+    return rt as ValueElement;
+}
