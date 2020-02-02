8e0010bf96f5c95ffc7f70e81cfd32760dca098d
Vladyslav Sitalo <sitalov@amazon.com>
2020-02-01 16:16:17 -0800

    Add selection functionality to RoamNode

--- src/ts/contentScripts/srs/index.tsx
+++ src/ts/contentScripts/srs/index.tsx
@@ -4,6 +4,9 @@ const bucketExpr = /\[\[Bucket (\d+)]]/;
 const nextBucket = (nodeStr: string) => `[[Bucket ${parseInt(nodeStr) + 1}]]`;
 
 export function triggerNextBucket() {
-    Roam.applyToCurrent((element =>
-        new RoamNode(element.text.replace(bucketExpr, (_, numStr: string) => nextBucket(numStr)))));
+    Roam.applyToCurrent(
+        (element => {
+            return new RoamNode(element.text.replace(bucketExpr, (_, numStr: string) => nextBucket(numStr)),
+                element.selection);
+        }));
 }
