12bc9efdeb395fbc4a325bc97ddaf4c35fcb2cfd
Vladyslav Sitalo <sitalov@amazon.com>
2020-02-08 20:53:09 -0800

    Introduce FuzzyDate functionality.

---
+++ src/@types/chrono-node/index.d.ts
@@ -0,0 +1,30 @@
+// Stolen from https://github.com/jungj5/Save-the-Date/blob/master/%40types/chrono-node/index.d.ts
+declare namespace chrono {
+    export class ParsedResult {
+        start: ParsedComponents;
+        end: ParsedComponents;
+        index: number;
+        text: string;
+        ref: Date;
+    }
+
+    export class ParsedComponents {
+        assign(component: string, value: number): void;
+
+        imply(component: string, value: number): void;
+
+        get(component: string): number;
+
+        isCertain(component: string): boolean;
+
+        date(): Date;
+    }
+
+    export function parseDate(text: string, refDate?: Date, opts?: any): Date;
+
+    export function parse(text: string, refDate?: Date, opts?: any): ParsedResult[];
+}
+
+declare module 'chrono-node' {
+    export = chrono;
+}
--- src/ts/contentScripts/dispatcher/index.ts
+++ src/ts/contentScripts/dispatcher/index.ts
@@ -1,6 +1,7 @@
 import {browser} from 'webextension-polyfill-ts';
 import {triggerNextBucket} from '../srs';
 import {Roam} from '../../utils/roam';
+import {guard, replaceFuzzyDate} from '../fuzzy_date';
 
 /**
  * Be cautious to reference functions on the objects via anonymous functions (e.g. see Roam.deleteBlock)
@@ -10,6 +11,11 @@ const dispatchMap = new Map([
     ['srs-next-bucket', triggerNextBucket],
     ['delete-current-block', () => Roam.deleteBlock()],
     ['duplicate-current-block', () => Roam.duplicateBlock()],
+    ['replace-fuzzy-date', replaceFuzzyDate],
 ]);
 
 browser.runtime.onMessage.addListener((command) => dispatchMap.get(command)?.());
+
+document.addEventListener('keyup', ev => {
+    if (ev.key === guard) replaceFuzzyDate();
+});
