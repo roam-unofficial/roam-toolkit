48a7d6e35ce1ecd3d134ed82c699314fd7fc65da
Piotr Skarżyński <piotr.skarzynski@gmail.com>
2020-02-09 23:55:52 +0100

    selected text duplication added

--- src/ts/contentScripts/duplicate-block-content/index.tsx
+++ src/ts/contentScripts/duplicate-block-content/index.tsx
@@ -1,5 +1,5 @@
 import { Feature } from '../../utils/settings'
-import { Roam } from '../../utils/roam';
+import { Roam, RoamNode, Selection } from '../../utils/roam';
 
 export const config: Feature = {
     id: 'duplicate',
@@ -7,7 +7,26 @@ export const config: Feature = {
     shortcuts: [
         {
             id: 'dupShortcut', label: 'Shortcut for duplication', initValue: '',
-            onPress: () => Roam.duplicateBlock()
+            onPress: () => duplicate()
         },
     ]
+}
+
+const duplicate = () => {
+    const node = Roam.getActiveRoamNode();
+    const selectedText = node?.selectedText();
+
+    if (node && selectedText) {
+        const selectionLength = selectedText.length;
+        const selectionEnd = node.selection.end
+        const newText = node.text.substring(0, selectionEnd)
+            + selectedText
+            + node.text.substring(selectionEnd);
+
+        Roam.applyToCurrent((() => {
+            return new RoamNode(newText, new Selection(selectionEnd, selectionEnd + selectionLength));
+        }));
+    } else {
+        Roam.duplicateBlock()
+    }
 }
