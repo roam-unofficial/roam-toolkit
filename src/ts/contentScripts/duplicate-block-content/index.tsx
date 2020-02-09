9d7c41f0b67b3c204ca794117aa10694f0d42b1f
Piotr Skarżyński <piotr.skarzynski@gmail.com>
2020-02-09 12:55:28 +0100

    duplicate change

--- src/ts/contentScripts/duplicate-block-content/index.tsx
+++ src/ts/contentScripts/duplicate-block-content/index.tsx
@@ -1,5 +1,6 @@
-import { getActiveEditElement } from '../../utils/dom';
+// import { getActiveEditElement } from '../../utils/dom';
 import { Feature } from '../../utils/settings'
+import { Roam } from '../../utils/roam';
 
 export const config: Feature = {
     id: 'duplicate',
@@ -7,7 +8,7 @@ export const config: Feature = {
     settings: [
         {
             type: 'shortcut', id: 'dupShortcut', label: 'Shortcut for duplication', initValue: '',
-            onPress: () => duplicate()
+            onPress: () => Roam.duplicateBlock()
         },
     ]
 }
@@ -29,47 +30,41 @@ export const config: Feature = {
 
 
 
-const pasteEvent = new Event('input', {
-    bubbles: true,
-    cancelable: true,
-});
-const inputEvent = new Event('input', {
-    bubbles: true,
-    cancelable: true,
-});
-
-const duplicate = () => {
-    const element = getActiveEditElement() as HTMLTextAreaElement;
-
-    if (element.nodeName === 'TEXTAREA') {
-        const blockContent = element.value;
-
-        if (element.selectionStart !== element.selectionEnd) { // duplicate selection
-            const contentToPaste = element.value.substring(element.selectionStart, element.selectionEnd);
-            const selectionLength = contentToPaste.length;
-            const selectionEnd = element.selectionEnd
-            element.value = element.value.substring(0, element.selectionEnd)
-                + contentToPaste
-                + element.value.substring(element.selectionEnd);
-            element.selectionStart = selectionEnd;
-            element.selectionEnd = selectionEnd + selectionLength;
-            element.dispatchEvent(inputEvent);
-        } else { // duplicate block
-            element.value = '';
-            element.dispatchEvent(inputEvent);
-            navigator.clipboard.readText().then(oldClipboardContent => {
-                navigator.clipboard.writeText(blockContent + '\r\n' + blockContent).then(() => {
-                    document.execCommand('paste');
-                    element.dispatchEvent(pasteEvent);
-                    navigator.clipboard.writeText(oldClipboardContent);
-                });
-            });
-        }
-    }
-}
-
-// browser.runtime.onMessage.addListener((command) => {
-//     if (command === 'duplicate-content') {
-//         duplicate();
-//     }
+// const pasteEvent = new Event('input', {
+//     bubbles: true,
+//     cancelable: true,
 // });
+// const inputEvent = new Event('input', {
+//     bubbles: true,
+//     cancelable: true,
+// });
+
+// const duplicate = () => {
+//     const element = getActiveEditElement() as HTMLTextAreaElement;
+
+//     if (element.nodeName === 'TEXTAREA') {
+//         const blockContent = element.value;
+
+//         if (element.selectionStart !== element.selectionEnd) { // duplicate selection
+//             const contentToPaste = element.value.substring(element.selectionStart, element.selectionEnd);
+//             const selectionLength = contentToPaste.length;
+//             const selectionEnd = element.selectionEnd
+//             element.value = element.value.substring(0, element.selectionEnd)
+//                 + contentToPaste
+//                 + element.value.substring(element.selectionEnd);
+//             element.selectionStart = selectionEnd;
+//             element.selectionEnd = selectionEnd + selectionLength;
+//             element.dispatchEvent(inputEvent);
+//         } else { // duplicate block
+//             element.value = '';
+//             element.dispatchEvent(inputEvent);
+//             navigator.clipboard.readText().then(oldClipboardContent => {
+//                 navigator.clipboard.writeText(blockContent + '\r\n' + blockContent).then(() => {
+//                     document.execCommand('paste');
+//                     element.dispatchEvent(pasteEvent);
+//                     navigator.clipboard.writeText(oldClipboardContent);
+//                 });
+//             });
+//         }
+//     }
+// }
