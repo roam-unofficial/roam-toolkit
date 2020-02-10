1426733aa62eb7d0cf5cd938024efea1a5de17b8
Piotr Skarżyński <piotr.skarzynski@gmail.com>
2020-02-10 13:56:14 +0100

    srs shortcut added to popup

--- src/ts/contentScripts/features.tsx
+++ src/ts/contentScripts/features.tsx
@@ -3,9 +3,11 @@ import { prepareSettings, Feature } from '../utils/settings'
 import { config as incDec } from './inc-dec-value/index'
 import { config as duplicate } from './duplicate-block-content/index'
 import { config as customCss } from './custom-css/index'
+import { config as srs } from './srs/index'
 
 export const features: Feature[] = prepareSettings([
     incDec,
     duplicate,
-    customCss
+    customCss,
+    srs
 ])
--- src/ts/contentScripts/srs/index.tsx
+++ src/ts/contentScripts/srs/index.tsx
@@ -1,4 +1,16 @@
-import {Roam, RoamNode} from '../../utils/roam';
+import { Roam, RoamNode } from '../../utils/roam';
+import { Feature } from '../../utils/settings'
+
+export const config: Feature = {
+    id: 'srs',
+    name: 'SRS',
+    shortcuts: [
+        {
+            id: 'nextBucketShortcut', label: 'Trigger next bucket', initValue: 'Ctrl+s',
+            onPress: triggerNextBucket
+        }
+    ]
+}
 
 const bucketExpr = /\[\[Bucket (\d+)]]/;
 const nextBucket = (nodeStr: string) => `[[Bucket ${parseInt(nodeStr) + 1}]]`;
