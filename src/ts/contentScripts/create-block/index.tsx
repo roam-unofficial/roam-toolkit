3648fe10ee462fa3a28d6528474f9165ddccecaf
Dalmo Mendonça <dalmo3@gmail.com>
2020-02-17 22:00:26 +1300

    some tests with css visibility

--- src/ts/contentScripts/create-block/index.tsx
+++ src/ts/contentScripts/create-block/index.tsx
@@ -1,7 +1,6 @@
 import { browser } from 'webextension-polyfill-ts';
 import {
   getTopLevelBlockList,
-  ValueElement,
   getInputEvent,
   simulateMouseClick,
   pressESC,
@@ -13,8 +12,8 @@ import {
 
 const create = async () => {
   //get last block from list
-  let lastChild = getTopLevelBlockList().lastChild as ValueElement;
-  let lastBlock = lastChild.querySelector('.roam-block') as ValueElement;
+  let lastChild = getTopLevelBlockList().lastChild as HTMLElement;
+  let lastBlock = lastChild.querySelector('.roam-block') as HTMLElement;
 
   // click on block to trigger textarea
   // not needed if lastblock is already selected
@@ -31,24 +30,38 @@ const create = async () => {
     console.log('element: ', element);
     console.log('value', element.value);
 
-    //move to the end of the line
+    //move selection to the end of the line
     element.setSelectionRange(
       element.value.length * 2,
       element.value.length * 2
     );
 
+    // add css rule to hide the next block before it is created
+    let ss = document.styleSheets[0] as CSSStyleSheet;
+    let numChildren = getTopLevelBlockList().children.length;
+    ss.insertRule(`.roam-article div .flex-v-box:nth-child(${numChildren+1}) {  width: 0px; overflow: hidden }`)
+
     // press enter to create new block
     await pressEnter();
     // ShiftTab guarantees the new block is a top level block
     await pressShiftTab();
+    
+    // hide it
+    // lastChild = getTopLevelBlockList().lastChild as HTMLElement;
+    // lastChild.style.visibility = 'hidden';
 
-    element = getActiveEditElement() as HTMLTextAreaElement;
+
+    // element = getActiveEditElement() as HTMLTextAreaElement;
+    lastChild = getTopLevelBlockList().lastChild as HTMLElement;
+    element = lastChild.querySelector('textarea') as HTMLTextAreaElement;
+  
 
     if (element.nodeName === 'TEXTAREA') {
       // const blockContent = element.value;
       console.log(element.id);
       // writing to active block
       element.value = 'new block' + element.id.split('-').pop();
+      
       // try testing with query
       //   element.value = ':q [:find ?a ?b :where [?a :node/title ?b]]';
       element.dispatchEvent(getInputEvent());
@@ -56,7 +69,7 @@ const create = async () => {
 
       // unfocus block
       // esc enters into block selection mode
-      await pressESC();
+      await pressESC(element);
 
       // resulting table from query
       const table = lastChild.querySelector('table');
@@ -68,9 +81,11 @@ const create = async () => {
 
       // block is selected, now either:
       // leave block selection mode with ESC
-      await pressESC();
+    //   await pressESC(element);
       // or delete block with Backspace
-      // await pressBackspace();
+      await pressBackspace();
+
+      ss.deleteRule(0);
     }
   }
 };
