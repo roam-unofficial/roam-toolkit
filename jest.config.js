001a8eec8b4a96bc8d25ae4e4c6e4ef434d51959
Vladyslav Sitalo <sitalov@amazon.com>
2020-02-23 17:09:34 -0800

    Introduce withDate mixin.

--- jest.config.js
+++ jest.config.js
@@ -124,7 +124,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ["jest-extended"],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
