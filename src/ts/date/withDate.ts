f8f0ad834a75cb22ee565810dab213403663e531
Vladyslav Sitalo <sitalov@amazon.com>
2020-02-23 17:09:34 -0800

    Introduce withDate mixin.

---
+++ src/ts/date/withDate.ts
@@ -0,0 +1,24 @@
+import {RoamNode} from '../utils/roam';
+import {RoamDate} from './common';
+
+
+type Constructor<T> = new (...args: any[]) => T;
+
+export function withDate<T extends Constructor<RoamNode>>(SuperClass: T) {
+    return class NodeWithDate extends SuperClass {
+        listDatePages() {
+            return this.text.match(RoamDate.regex) || []
+        }
+
+        /** If has 1 date - replace it, if more then 1 date - append it */
+        withDate(date: Date) {
+            const currentDates = this.listDatePages()
+            const newDate = RoamDate.format(date);
+            const newText = currentDates.length === 1 ?
+                this.text.replace(currentDates[0], newDate) :
+                this.text + ' ' + newDate;
+
+            return new NodeWithDate(newText, this.selection)
+        }
+    };
+}
---
+++ tests/ts/date/withDate.test.ts
@@ -0,0 +1,44 @@
+import {withDate} from '../../../src/ts/date/withDate';
+import {RoamNode} from '../../../src/ts/utils/roam';
+import {RoamDate} from '../../../src/ts/date/common';
+
+const NodeWitDate = withDate(RoamNode)
+
+describe(NodeWitDate, () => {
+    const datePage1 = `[[February 23rd, 2020]]`;
+    const datePage2 = `[[February 24th, 2020]]`;
+
+    const multiDateNode = new NodeWitDate(`test ${datePage1} ${datePage2}`)
+    const noDateNode = new NodeWitDate('test')
+
+    describe('listDatePages', () => {
+        test('return list of dates when multiple are present', () => {
+            expect(multiDateNode.listDatePages()).toStrictEqual([datePage1, datePage2])
+        })
+
+        test('return empty array when no dates found', () => {
+            expect(noDateNode.listDatePages()).toHaveLength(0)
+        })
+    })
+
+    describe('withDate', () => {
+        const newDate = new Date(2020, 2, 22);
+        test('when multiple dates => append new one at the end', () => {
+            expect(multiDateNode.withDate(newDate).text).toEndWith(RoamDate.format(newDate))
+        })
+
+        test('when no date => append new one at the end', () => {
+            expect(noDateNode.withDate(newDate).text).toEndWith(RoamDate.format(newDate))
+        })
+
+        test('when 1 date => replace it with new one', () => {
+            const oneDateNode = new NodeWitDate(`test ${datePage1} continues`)
+
+            const newNode = oneDateNode.withDate(newDate);
+            const dateStr = RoamDate.format(newDate);
+
+            expect(newNode.text).not.toEndWith(dateStr)
+            expect(newNode.listDatePages()).toContain(dateStr)
+        })
+    })
+})
