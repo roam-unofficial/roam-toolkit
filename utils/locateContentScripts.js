573e00f44f31c4db7aab029ebe8505d74c0535cc
Palash <palash@locus.sh>
2020-05-10 02:20:35 +0530

    run prettier for .js files

--- utils/locateContentScripts.js
+++ utils/locateContentScripts.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const { readdirSync, lstatSync } = require('fs');
-const { red, yellow, reset, bold, blink, dim} = require('./consoleColors');
+const path = require('path')
+const {readdirSync, lstatSync} = require('fs')
+const {red, yellow, reset, bold, blink, dim} = require('./consoleColors')
 
-const isDirectory = (source) => {
-    return lstatSync(source).isDirectory();
+const isDirectory = source => {
+    return lstatSync(source).isDirectory()
 }
 
 /**
@@ -11,19 +11,17 @@ const isDirectory = (source) => {
  *                        Throws error if more than one index file is present in the folder with the supported extensions.
  *                        returns undefined if no file exists.
  * @param contentPath - String representaion of the content path.
- * @returns - The index file name if exists in the given path. 
+ * @returns - The index file name if exists in the given path.
  */
-const findContentEntryFile = (contentPath) => {
-    const files = readdirSync(contentPath).filter((name) => /index.(js|ts|tsx)/.test(name));
+const findContentEntryFile = contentPath => {
+    const files = readdirSync(contentPath).filter(name => /index.(js|ts|tsx)/.test(name))
     if (!files || files.length < 1) {
-        console.log(`${yellow}Could not find any index files in path:${reset}\n${contentPath}\n`);
-        return undefined;
-    }
-    else if (files.length == 1) {
-        return files.pop();
-    }
-    else {
-        throw `${red}Found ${files.length} index files, ${files}, in path:${reset}\n${contentPath}\nOnly one should be set.\n`;
+        console.log(`${yellow}Could not find any index files in path:${reset}\n${contentPath}\n`)
+        return undefined
+    } else if (files.length == 1) {
+        return files.pop()
+    } else {
+        throw `${red}Found ${files.length} index files, ${files}, in path:${reset}\n${contentPath}\nOnly one should be set.\n`
     }
 }
 
@@ -34,25 +32,27 @@ const findContentEntryFile = (contentPath) => {
  * @param rootPath - String representaion of the root path.
  * @returns - A list of objects containing path and folder name
  */
-const getDirectories = (rootPath) => {
+const getDirectories = rootPath => {
     return readdirSync(rootPath)
         .map(name => {
             return {
                 path: path.join(rootPath, name),
-                folderName: name
+                folderName: name,
             }
         })
-        .filter((dir) => isDirectory(dir.path));
+        .filter(dir => isDirectory(dir.path))
 }
 
-const locateContentScripts = (rootPath) => {
-    const entries = {};
-    const directories = getDirectories(rootPath);
-    directories.forEach((dir) => {
-        const entryFile = findContentEntryFile(dir.path);
-        if (entryFile) { entries[dir.folderName] = path.join(dir.path, entryFile) }
+const locateContentScripts = rootPath => {
+    const entries = {}
+    const directories = getDirectories(rootPath)
+    directories.forEach(dir => {
+        const entryFile = findContentEntryFile(dir.path)
+        if (entryFile) {
+            entries[dir.folderName] = path.join(dir.path, entryFile)
+        }
     })
-    return entries;
+    return entries
 }
 
-module.exports = locateContentScripts;
+module.exports = locateContentScripts
--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,98 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { CheckerPlugin } = require('awesome-typescript-loader');
-const ExtensionReloader = require('webpack-extension-reloader');
-const locateContentScripts = require('./utils/locateContentScripts');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const {CheckerPlugin} = require('awesome-typescript-loader')
+const ExtensionReloader = require('webpack-extension-reloader')
+const locateContentScripts = require('./utils/locateContentScripts')
 
-const sourceRootPath = path.join(__dirname, 'src');
-const contentScriptsPath = path.join(sourceRootPath, 'ts', 'contentScripts');
-const distRootPath = path.join(__dirname, 'dist');
-const nodeEnv = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
-const webBrowser = process.env.WEB_BROWSER ? process.env.WEB_BROWSER : 'chrome';
+const sourceRootPath = path.join(__dirname, 'src')
+const contentScriptsPath = path.join(sourceRootPath, 'ts', 'contentScripts')
+const distRootPath = path.join(__dirname, 'dist')
+const nodeEnv = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
+const webBrowser = process.env.WEB_BROWSER ? process.env.WEB_BROWSER : 'chrome'
 
-const contentScripts = locateContentScripts(contentScriptsPath);
+const contentScripts = locateContentScripts(contentScriptsPath)
 
-const extensionReloader = nodeEnv === 'watch' ? new ExtensionReloader({
-	port: 9128,
-	reloadPage: true,
-	entries: {
-		background: 'background',
-		extensionPage: ['popup', 'options'],
-		contentScript: Object.keys(contentScripts),
-	}
-}) : () => { this.apply = () => { } };
+const extensionReloader =
+    nodeEnv === 'watch'
+        ? new ExtensionReloader({
+              port: 9128,
+              reloadPage: true,
+              entries: {
+                  background: 'background',
+                  extensionPage: ['popup', 'options'],
+                  contentScript: Object.keys(contentScripts),
+              },
+          })
+        : () => {
+              this.apply = () => {}
+          }
 
-const cleanWebpackPlugin = nodeEnv === 'production' ? new CleanWebpackPlugin() : () => { this.apply = () => { } };
+const cleanWebpackPlugin =
+    nodeEnv === 'production'
+        ? new CleanWebpackPlugin()
+        : () => {
+              this.apply = () => {}
+          }
 
 module.exports = {
-	watch: nodeEnv === 'watch',
-	entry: {
-		background: path.join(sourceRootPath, 'ts', 'background', 'index.ts'),
-		options: path.join(sourceRootPath, 'ts', 'options', 'index.tsx'),
-		popup: path.join(sourceRootPath, 'ts', 'popup', 'index.tsx'),
-		...contentScripts,
-	},
-	output: {
-		path: distRootPath,
-		filename: '[name].js',
-	},
-	resolve: {
-		extensions: ['.js', '.ts', '.tsx', '.json'],
-	},
-	module: {
-		rules: [
-			{ test: /\.(js|ts|tsx)?$/, loader: 'awesome-typescript-loader', exclude: /node_modules/ },
-		]
-	},
-	plugins: [
-		new CheckerPlugin(),
-		new HtmlWebpackPlugin({
-			template: path.join(sourceRootPath, 'html', 'options.html'),
-			inject: 'body',
-			filename: 'options.html',
-			title: 'Web Extension Starter - Options Page',
-			chunks: ['options'],
-		}),
-		new HtmlWebpackPlugin({
-			template: path.join(sourceRootPath, 'html', 'popup.html'),
-			inject: 'body',
-			filename: 'popup.html',
-			title: 'Web Extension Starter - Popup Page',
-			chunks: ['popup'],
-		}),
-		new CopyWebpackPlugin([
-			{
-				from: path.join(sourceRootPath, 'assets'),
-				to: path.join(distRootPath, 'assets'),
-				test: /\.(jpg|jpeg|png|gif|svg)?$/,
-			},
-			{
-				from: path.join(sourceRootPath, 'manifest.json'),
-				to: path.join(distRootPath, 'manifest.json'),
-				toType: 'file',
-			}
-		],{
-			copyUnmodified: true // resolve conflict with `CleanWebpackPlugin`
-		}),
-		new webpack.DefinePlugin({
-			'NODE_ENV': JSON.stringify(nodeEnv),
-			'WEB_BROWSER': JSON.stringify(webBrowser),
-		}),
-		extensionReloader,
-		new CleanWebpackPlugin(),
-	],
+    watch: nodeEnv === 'watch',
+    entry: {
+        background: path.join(sourceRootPath, 'ts', 'background', 'index.ts'),
+        options: path.join(sourceRootPath, 'ts', 'options', 'index.tsx'),
+        popup: path.join(sourceRootPath, 'ts', 'popup', 'index.tsx'),
+        ...contentScripts,
+    },
+    output: {
+        path: distRootPath,
+        filename: '[name].js',
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+    },
+    module: {
+        rules: [{test: /\.(js|ts|tsx)?$/, loader: 'awesome-typescript-loader', exclude: /node_modules/}],
+    },
+    plugins: [
+        new CheckerPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(sourceRootPath, 'html', 'options.html'),
+            inject: 'body',
+            filename: 'options.html',
+            title: 'Web Extension Starter - Options Page',
+            chunks: ['options'],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(sourceRootPath, 'html', 'popup.html'),
+            inject: 'body',
+            filename: 'popup.html',
+            title: 'Web Extension Starter - Popup Page',
+            chunks: ['popup'],
+        }),
+        new CopyWebpackPlugin(
+            [
+                {
+                    from: path.join(sourceRootPath, 'assets'),
+                    to: path.join(distRootPath, 'assets'),
+                    test: /\.(jpg|jpeg|png|gif|svg)?$/,
+                },
+                {
+                    from: path.join(sourceRootPath, 'manifest.json'),
+                    to: path.join(distRootPath, 'manifest.json'),
+                    toType: 'file',
+                },
+            ],
+            {
+                copyUnmodified: true, // resolve conflict with `CleanWebpackPlugin`
+            }
+        ),
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(nodeEnv),
+            WEB_BROWSER: JSON.stringify(webBrowser),
+        }),
+        extensionReloader,
+        new CleanWebpackPlugin(),
+    ],
 }
