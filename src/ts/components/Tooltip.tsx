806203de2b3ea4dbda73904e645b4a48371f8c45
Palash <palash@locus.sh>
2020-05-16 14:40:30 +0530

    refactor Tooltip

--- src/ts/components/Tooltip.tsx
+++ src/ts/components/Tooltip.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import TooltipTrigger from 'react-popper-tooltip'
+import TooltipTrigger, {ChildrenArg, TooltipArg} from 'react-popper-tooltip'
 import styled from 'styled-components'
 
 type Props = {
@@ -12,34 +12,38 @@ const StyledTooltipContent = styled('div')`
     color: white;
     padding: 10px;
 `
-const Tooltip = ({children, tooltip, hideArrow, ...props}: Props) => {
+function renderTooltipContent(tooltip: string) {
+    return ({tooltipRef, getTooltipProps}: TooltipArg) => (
+        <StyledTooltipContent
+            {...getTooltipProps({
+                ref: tooltipRef,
+                className: 'tooltip-container',
+            })}
+        >
+            {tooltip}
+        </StyledTooltipContent>
+    )
+}
+function renderTooltipTrigger(children: any) {
+    return ({getTriggerProps, triggerRef}: ChildrenArg) => (
+        <span
+            {...getTriggerProps({
+                ref: triggerRef,
+                className: 'trigger',
+            })}
+        >
+            {children}
+        </span>
+    )
+}
+
+const Tooltip = ({children, tooltip, ...props}: Props) => {
     if (!tooltip) {
         return children
     }
     return (
-        <TooltipTrigger
-            {...props}
-            tooltip={({tooltipRef, getTooltipProps}) => (
-                <StyledTooltipContent
-                    {...getTooltipProps({
-                        ref: tooltipRef,
-                        className: 'tooltip-container',
-                    })}
-                >
-                    {tooltip}
-                </StyledTooltipContent>
-            )}
-        >
-            {({getTriggerProps, triggerRef}) => (
-                <span
-                    {...getTriggerProps({
-                        ref: triggerRef,
-                        className: 'trigger',
-                    })}
-                >
-                    {children}
-                </span>
-            )}
+        <TooltipTrigger {...props} tooltip={renderTooltipContent(tooltip)}>
+            {renderTooltipTrigger(children)}
         </TooltipTrigger>
     )
 }
