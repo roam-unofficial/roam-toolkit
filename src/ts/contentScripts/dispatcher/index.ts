ac2a875bcaeb61490d1b4d6903d986510b58ca21
thesved <2893181+thesved@users.noreply.github.com>
2020-05-16 18:39:55 +0200

    Comment added for replaceFuzzyDate triggering

--- src/ts/contentScripts/dispatcher/index.ts
+++ src/ts/contentScripts/dispatcher/index.ts
@@ -20,6 +20,13 @@ const dispatchMap = new Map([
 
 browser.runtime.onMessage.addListener(command => dispatchMap.get(command)?.())
 
+/**
+ * We use `keypress`, since `keyup` is sometimes firing for individual keys instead of the pressed key 
+ * when the guard character is requiring a multi-key stroke.
+ *
+ * `setTimeout` is used to put the callback to the end of the event queue, 
+ * since the input is not yet changed when keypress is firing.
+ */
 document.addEventListener('keypress', ev => {
     if (ev.key === guard) setTimeout(replaceFuzzyDate, 0)
 })
