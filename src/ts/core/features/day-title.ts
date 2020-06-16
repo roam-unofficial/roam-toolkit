62540ab93880324ee1e915288f8df4e768b8d52a
Braydie Grove <braydie.grove@gmail.com>
2020-06-16 14:28:55 +0100

    Added mouseover event for page titles to show day name

---
+++ src/ts/core/features/day-title.ts
@@ -0,0 +1,39 @@
+import {RoamDate} from '../roam/date'
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+function getDayFromDate(name: string) {
+    let re = /(.*) (\d+).{2}, (\d{4})/i
+    var matches = name.match(re)
+    if (matches && matches.length === 4) {
+        const d = RoamDate.parse(name)
+        return days[d.getDay()]
+    }
+    return null
+}
+
+function isPageViewTitle(element: HTMLElement) {
+    return (
+        element.parentNode &&
+        element.parentNode.parentNode &&
+        (element.parentNode.parentNode as Element).classList.contains('rm-ref-page-view-title')
+    )
+}
+
+document.querySelector('body')?.addEventListener('mouseover', ev => {
+    let day = null
+    const target = ev.target as HTMLElement
+
+    if (target === null) {
+        return
+    }
+
+    if (target.classList.contains('rm-page-ref') || isPageViewTitle(target)) {
+        day = getDayFromDate(target.innerText)
+    }
+
+    if (day == null) {
+        return
+    }
+    target.setAttribute('title', day)
+})
