d692ceddcdb07fef8fea35043dd5cab8372af20d
Dave Lu <davelu@yelp.com>
2020-06-24 21:08:33 -0700

    Allow absolute imports in jest tests

--- jest.config.js
+++ jest.config.js
@@ -69,7 +69,9 @@ module.exports = {
     moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
 
     // A map from regular expressions to module names that allow to stub out resources with a single module
-    // moduleNameMapper: {},
+    moduleNameMapper: {
+        'src/(.*)': '<rootDir>/src/ts/$1'
+    },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
