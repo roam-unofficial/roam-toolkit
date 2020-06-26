b35fe5e1c460bbcd313d171a10a5446423366421
Dave Lu <davelu@yelp.com>
2020-06-26 12:00:35 -0700

    Remove unnecessary newline

--- src/ts/core/common/assert.ts
+++ src/ts/core/common/assert.ts
@@ -1,4 +1,3 @@
-
 export const assumeExists = <T>(x: T | null | undefined, errorMessage='Assumed that variable exists, but it does not'): T => {
     if (!x) {
         throw new Error(errorMessage);
