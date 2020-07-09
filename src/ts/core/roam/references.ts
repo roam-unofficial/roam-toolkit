5a88a00b3ee484b8e68ace1231334c9e39a2781a
Vladyslav Sitalo <sitalov@amazon.com>
2020-07-08 21:34:04 -0700

    Add mapping to collapse the page in the reference/query view

--- src/ts/core/roam/references.ts
+++ src/ts/core/roam/references.ts
@@ -1,13 +1,17 @@
-import {Selectors} from './selectors';
-import {Mouse} from '../common/mouse';
-import {RoamBlock} from '../features/vim-mode/roam/roam-block';
+import {Selectors} from './selectors'
+import {Mouse} from '../common/mouse'
+import {RoamBlock} from '../features/vim-mode/roam/roam-block'
 
 export const expandLastBreadcrumb = () => {
     const referenceItem = RoamBlock.selected().element?.closest(Selectors.referenceItem)
-    if (!referenceItem) return
+    const breadcrumbs = referenceItem?.querySelector(Selectors.breadcrumbsContainer)
 
-    const breadcrumbs = referenceItem.querySelector(Selectors.breadcrumbsContainer)
-    if (!breadcrumbs) return
+    if (breadcrumbs?.lastElementChild) Mouse.leftClick(breadcrumbs.lastElementChild as HTMLElement)
+}
 
-    if (breadcrumbs.lastElementChild) Mouse.leftClick(breadcrumbs.lastElementChild as HTMLElement)
+export const closePageReferenceView = () => {
+    const referenceItem = RoamBlock.selected().element?.closest(Selectors.pageReferenceItem)
+    const caretButton = referenceItem?.querySelector(Selectors.caretButton)
+
+    if (caretButton) Mouse.leftClick(caretButton as HTMLElement)
 }
