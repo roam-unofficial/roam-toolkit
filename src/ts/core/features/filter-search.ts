42b94ead42f43b962ed97f1906314e169eab1b35
Vladyslav Sitalo <sitalov@amazon.com>
2020-07-11 16:56:26 -0700

    Filter-Search: Restructure to split into smaller units

--- src/ts/core/features/filter-search.ts
+++ src/ts/core/features/filter-search.ts
@@ -4,7 +4,7 @@ import {waitForSelectorToExist} from 'src/core/common/mutation-observer'
 
 export const config: Feature = {
     id: 'filter_search',
-    name: 'Search in page/reference filter',
+    name: 'Search pages in page or references filter',
     enabledByDefault: true,
 }
 
@@ -29,79 +29,102 @@ const toggleFilterSearch = (active: boolean) => {
     }
 }
 
+const searchInputId = 'searchInputBox'
+
+const getSearchInput = (): HTMLInputElement | null => {
+    return document.getElementById(searchInputId) as HTMLInputElement
+}
+
 async function filterSearch(event: MouseEvent) {
     const eventTarget = event.target as HTMLElement
-    if (eventTarget !== null) {
-        console.log(eventTarget.className)
+    if (!eventTarget) return
 
-        if (eventTarget.className === 'bp3-button') {
-            const tbInputSearch = document.getElementById('fbSearchInput') as HTMLInputElement
-            if (tbInputSearch !== null) {
-                tbInputSearch.focus()
-                tbInputSearch.select()
-            }
-        } else if (
-            eventTarget.className === 'bp3-icon bp3-icon-filter' ||
-            eventTarget.className === 'bp3-button bp3-minimal bp3-small'
-        ) {
-            const filterContainer = await waitForSelectorToExist(
-                'div.bp3-transition-container.bp3-popover-enter-done div.bp3-popover-content > div > div'
-            )
-            const newDivLine = document.createElement('div')
-            newDivLine.className = 'rm-line'
-            filterContainer.prepend(newDivLine)
+    const filterPageButtonClicked = eventTarget.className === 'bp3-button'
+    const filterActivationButtonClicked =
+        eventTarget.className === 'bp3-icon bp3-icon-filter' ||
+        eventTarget.className === 'bp3-button bp3-minimal bp3-small'
 
-            const newDiv = document.createElement('div')
-            newDiv.id = 'filterBoxSearch'
-            newDiv.style.cssText = 'display:flex;padding:4px'
-            filterContainer.prepend(newDiv)
-
-            const newLabel = document.createElement('strong')
-            newLabel.innerText = 'Search'
-            newLabel.style.cssText = 'margin-right:10px'
-            newDiv.appendChild(newLabel)
-
-            const newInput = document.createElement('input')
-            newInput.value = ''
-            newInput.id = 'fbSearchInput'
-            newInput.name = 'fbSearchInput'
-            newInput.style.cssText = 'width:200px;display:flex'
-            newDiv.appendChild(newInput)
-            newInput.focus()
-
-            newInput.addEventListener('input', newInputClick)
-            return
-        } else {
-            console.log('Filter box was not loaded in time!')
-        }
+    if (filterPageButtonClicked) {
+        refocusSearchInput()
+    } else if (filterActivationButtonClicked) {
+        await createSearchFilterElements()
     }
 }
 
-function newInputClick() {
-    const inputTxtVal = (document.getElementById('fbSearchInput') as HTMLInputElement).value.toString()
-    console.log(inputTxtVal)
+function refocusSearchInput() {
+    const searchInput = getSearchInput()
+    if (searchInput) {
+        searchInput.focus()
+        searchInput.select()
+        onFilterInput()
+    }
+}
 
-    // Get filter box (only works when opened)
-    const allByClass2 = document.querySelectorAll('.bp3-overlay.bp3-overlay-open.bp3-overlay-inline')
-    const filterBox = allByClass2[0]
-    if (typeof filterBox !== 'undefined') {
-        const allFilterButtons = filterBox.querySelectorAll('div:not(.flex-h-box) > div > button.bp3-button')
+async function createSearchFilterElements() {
+    const filterContainer = await waitForSelectorToExist(
+        'div.bp3-transition-container.bp3-popover-enter-done div.bp3-popover-content > div > div'
+    )
 
-        for (let i = 0; i < allFilterButtons.length; i++) {
-            const curElement = allFilterButtons.item(i) as HTMLElement
+    function createSeparator() {
+        const newDivLine = document.createElement('div')
+        newDivLine.className = 'rm-line'
+        filterContainer.prepend(newDivLine)
+    }
 
-            if (inputTxtVal !== '') {
-                const curElemText = curElement.innerText.toString().toLowerCase()
-                if (curElemText.indexOf(inputTxtVal.toLowerCase()) > -1) {
-                    curElement.style.display = 'inline-flex'
-                } else {
-                    curElement.style.display = 'none'
-                }
-            } else {
-                curElement.style.display = 'inline-flex'
-            }
-        }
+    createSeparator()
+
+    function createSearchElementsContainer() {
+        const container = document.createElement('div')
+        container.id = 'filterBoxSearch'
+        container.style.cssText = 'display:flex;padding:4px'
+        filterContainer.prepend(container)
+        return container
+    }
+
+    const searchElementsContainer = createSearchElementsContainer()
+
+    function createSearchLabel() {
+        const searchLabel = document.createElement('strong')
+        searchLabel.innerText = 'Search'
+        searchLabel.style.cssText = 'margin-right:10px'
+        searchElementsContainer.appendChild(searchLabel)
+    }
+
+    createSearchLabel()
+
+    function createSearchInput() {
+        const searchInput = document.createElement('input')
+        searchInput.value = ''
+        searchInput.id = searchInputId
+        searchInput.name = searchInputId
+        searchInput.style.cssText = 'width:200px;display:flex'
+        searchElementsContainer.appendChild(searchInput)
+
+        searchInput.addEventListener('input', onFilterInput)
+    }
+
+    createSearchInput()
+    refocusSearchInput()
+}
+
+function onFilterInput() {
+    const filterButtonsContainer = document.querySelector('.bp3-overlay.bp3-overlay-open.bp3-overlay-inline')
+    const filterButtons =
+        filterButtonsContainer?.querySelectorAll('div:not(.flex-h-box) > div > button.bp3-button') || []
+
+    const searchText = getSearchInput()?.value?.toString()?.toLowerCase()
+
+    filterButtons.forEach((button: Element) => updateButtonVisibility(button as HTMLElement, searchText))
+}
+
+function updateButtonVisibility(button: HTMLElement, searchText: string | undefined) {
+    const show = () => (button.style.display = 'inline-flex')
+    const hide = () => (button.style.display = 'none')
+
+    const buttonText = button.innerText.toString().toLowerCase()
+    if (searchText && !buttonText.includes(searchText)) {
+        hide()
     } else {
-        console.log('filter box is NOT open')
+        show()
     }
 }
