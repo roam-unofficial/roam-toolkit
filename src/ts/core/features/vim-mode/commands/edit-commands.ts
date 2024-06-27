005c6170895a016eb9c0656d740d0a4d2104ef63
Vladyslav Sitalo <sitalov@amazon.com>
2024-06-26 23:27:40 -0400

    Add vim normal mode inc/dec dates

--- src/ts/core/features/vim-mode/commands/edit-commands.ts
+++ src/ts/core/features/vim-mode/commands/edit-commands.ts
@@ -5,6 +5,8 @@ import {AnkiScheduler} from 'src/core/srs/AnkiScheduler'
 import {SM2Node} from 'src/core/srs/SM2Node'
 import {RoamDb} from 'src/core/roam/roam-db'
 import {getBlockUid} from 'src/core/roam/block'
+import {RoamDate} from 'src/core/roam/date'
+import {createModifier, modifyDate} from 'src/core/features/inc-dec-value'
 
 const getBlockText = (uid: string): string => {
     const block = RoamDb.getBlockByUid(uid)
@@ -29,9 +31,27 @@ const markDone = () => {
     RoamDb.updateBlockText(uid, '{{[[DONE]]}} ' + originalText)
 }
 
+const modifyBlockDate = (modifier: (input: number) => number) => {
+    const uid = selectedUid()
+    const originalText = getBlockText(uid)
+
+    const datesInContent = originalText.match(RoamDate.regex)
+    if (!datesInContent || datesInContent.length !== 1) return
+
+    RoamDb.updateBlockText(
+        uid,
+        originalText.replace(
+            datesInContent[0],
+            RoamDate.formatPage(modifyDate(RoamDate.parseFromReference(datesInContent[0]), modifier))
+        )
+    )
+}
+
 export const EditCommands = [
     nmap('cmd+enter', 'Mark done', markDone),
     ...SRSSignals.map(it =>
         nmap(`ctrl+shift+${it}`, `Reschedule Current Note (${SRSSignal[it]})`, () => rescheduleSelectedNote(it))
     ),
+    nmap('ctrl+alt+up', 'Increment Date', () => modifyBlockDate(createModifier(1))),
+    nmap('ctrl+alt+down', 'Decrement Date', () => modifyBlockDate(createModifier(-1))),
 ]
